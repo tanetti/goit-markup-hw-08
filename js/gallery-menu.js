0df8173bd8b6c11e3bc6f39f7ddc3e1e0e06cf81
Volodymyr Dudka <tanetti22@gmail.com>
2022-05-28 12:13:24 +0300

    Довавляет анимацию элементам фильтра

--- js/gallery-menu.js
+++ js/gallery-menu.js
@@ -26,7 +26,40 @@
   refs.designBtn.addEventListener('click', toggleChecked_design);
   refs.marketingBtn.addEventListener('click', toggleChecked_marketing);
 
-  function toggleChecked_all() {
+  function hideAll() {
+    if (refs.webA.classList.contains('gallery-list__item--hidden') === false) {
+      refs.webA.classList.toggle('gallery-list__item--hidden');
+    }
+    if (refs.webB.classList.contains('gallery-list__item--hidden') === false) {
+      refs.webB.classList.toggle('gallery-list__item--hidden');
+    }
+
+    if (refs.designA.classList.contains('gallery-list__item--hidden') === false) {
+      refs.designA.classList.toggle('gallery-list__item--hidden');
+    }
+    if (refs.designB.classList.contains('gallery-list__item--hidden') === false) {
+      refs.designB.classList.toggle('gallery-list__item--hidden');
+    }
+
+    if (refs.appA.classList.contains('gallery-list__item--hidden') === false) {
+      refs.appA.classList.toggle('gallery-list__item--hidden');
+    }
+    if (refs.appB.classList.contains('gallery-list__item--hidden') === false) {
+      refs.appB.classList.toggle('gallery-list__item--hidden');
+    }
+    if (refs.appC.classList.contains('gallery-list__item--hidden') === false) {
+      refs.appC.classList.toggle('gallery-list__item--hidden');
+    }
+
+    if (refs.marketingA.classList.contains('gallery-list__item--hidden') === false) {
+      refs.marketingA.classList.toggle('gallery-list__item--hidden');
+    }
+    if (refs.marketingB.classList.contains('gallery-list__item--hidden') === false) {
+      refs.marketingB.classList.toggle('gallery-list__item--hidden');
+    }
+  }
+
+  function showAll() {
     if (refs.webA.classList.contains('gallery-list__item--hidden')) {
       refs.webA.classList.toggle('gallery-list__item--hidden');
     }
@@ -57,157 +90,104 @@
     if (refs.marketingB.classList.contains('gallery-list__item--hidden')) {
       refs.marketingB.classList.toggle('gallery-list__item--hidden');
     }
+  }
+
+  function toggleChecked_all() {
+    hideAll();
+    setTimeout(showAll, 10);
 
     checkedBtn = document.querySelector('[checked]');
     checkedBtn.toggleAttribute('checked');
+    checkedBtn.toggleAttribute('disabled');
+
     refs.allBtn.toggleAttribute('checked');
+    refs.allBtn.toggleAttribute('disabled');
   }
 
   function toggleChecked_web() {
-    if (refs.webA.classList.contains('gallery-list__item--hidden')) {
-      refs.webA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.webB.classList.contains('gallery-list__item--hidden')) {
-      refs.webB.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.designA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.designB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designB.classList.toggle('gallery-list__item--hidden');
+    function showWeb() {
+      if (refs.webA.classList.contains('gallery-list__item--hidden')) {
+        refs.webA.classList.toggle('gallery-list__item--hidden');
+      }
+      if (refs.webB.classList.contains('gallery-list__item--hidden')) {
+        refs.webB.classList.toggle('gallery-list__item--hidden');
+      }
     }
 
-    if (refs.appA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appB.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appC.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appC.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.marketingA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.marketingB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingB.classList.toggle('gallery-list__item--hidden');
-    }
+    hideAll();
+    setTimeout(showWeb, 10);
 
     checkedBtn = document.querySelector('[checked]');
     checkedBtn.toggleAttribute('checked');
+    checkedBtn.toggleAttribute('disabled');
+
     refs.webBtn.toggleAttribute('checked');
+    refs.webBtn.toggleAttribute('disabled');
   }
 
   function toggleChecked_app() {
-    if (refs.webA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.webB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webB.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.designA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.designB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designB.classList.toggle('gallery-list__item--hidden');
+    function showApp() {
+      if (refs.appA.classList.contains('gallery-list__item--hidden')) {
+        refs.appA.classList.toggle('gallery-list__item--hidden');
+      }
+      if (refs.appB.classList.contains('gallery-list__item--hidden')) {
+        refs.appB.classList.toggle('gallery-list__item--hidden');
+      }
+      if (refs.appC.classList.contains('gallery-list__item--hidden')) {
+        refs.appC.classList.toggle('gallery-list__item--hidden');
+      }
     }
 
-    if (refs.appA.classList.contains('gallery-list__item--hidden')) {
-      refs.appA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appB.classList.contains('gallery-list__item--hidden')) {
-      refs.appB.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appC.classList.contains('gallery-list__item--hidden')) {
-      refs.appC.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.marketingA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.marketingB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingB.classList.toggle('gallery-list__item--hidden');
-    }
+    hideAll();
+    setTimeout(showApp, 10);
 
     checkedBtn = document.querySelector('[checked]');
     checkedBtn.toggleAttribute('checked');
+    checkedBtn.toggleAttribute('disabled');
+
     refs.appBtn.toggleAttribute('checked');
+    refs.appBtn.toggleAttribute('disabled');
   }
 
   function toggleChecked_design() {
-    if (refs.webA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.webB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webB.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.designA.classList.contains('gallery-list__item--hidden')) {
-      refs.designA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.designB.classList.contains('gallery-list__item--hidden')) {
-      refs.designB.classList.toggle('gallery-list__item--hidden');
+    function showDesign() {
+      if (refs.designA.classList.contains('gallery-list__item--hidden')) {
+        refs.designA.classList.toggle('gallery-list__item--hidden');
+      }
+      if (refs.designB.classList.contains('gallery-list__item--hidden')) {
+        refs.designB.classList.toggle('gallery-list__item--hidden');
+      }
     }
 
-    if (refs.appA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appB.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appC.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appC.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.marketingA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.marketingB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.marketingB.classList.toggle('gallery-list__item--hidden');
-    }
+    hideAll();
+    setTimeout(showDesign, 10);
 
     checkedBtn = document.querySelector('[checked]');
     checkedBtn.toggleAttribute('checked');
+    checkedBtn.toggleAttribute('disabled');
+
     refs.designBtn.toggleAttribute('checked');
+    refs.designBtn.toggleAttribute('disabled');
   }
 
   function toggleChecked_marketing() {
-    if (refs.webA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.webB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.webB.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.designA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.designB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.designB.classList.toggle('gallery-list__item--hidden');
+    function showMarketing() {
+      if (refs.marketingA.classList.contains('gallery-list__item--hidden')) {
+        refs.marketingA.classList.toggle('gallery-list__item--hidden');
+      }
+      if (refs.marketingB.classList.contains('gallery-list__item--hidden')) {
+        refs.marketingB.classList.toggle('gallery-list__item--hidden');
+      }
     }
 
-    if (refs.appA.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appB.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appB.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.appC.classList.contains('gallery-list__item--hidden') === false) {
-      refs.appC.classList.toggle('gallery-list__item--hidden');
-    }
-
-    if (refs.marketingA.classList.contains('gallery-list__item--hidden')) {
-      refs.marketingA.classList.toggle('gallery-list__item--hidden');
-    }
-    if (refs.marketingB.classList.contains('gallery-list__item--hidden')) {
-      refs.marketingB.classList.toggle('gallery-list__item--hidden');
-    }
+    hideAll();
+    setTimeout(showMarketing, 10);
 
     checkedBtn = document.querySelector('[checked]');
     checkedBtn.toggleAttribute('checked');
+    checkedBtn.toggleAttribute('disabled');
+
     refs.marketingBtn.toggleAttribute('checked');
+    refs.marketingBtn.toggleAttribute('disabled');
   }
 })();
